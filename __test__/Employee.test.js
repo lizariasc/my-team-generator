ae4a9569141d6a2b8df35120aa38802612317162
LAgrl <lizethariasc14@gmail.com>
2021-06-18 21:53:30 -0700

    name, id, email and getName tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,6 +1,31 @@
 const Employee = require("../lib/Employee");
 
-test("Create a new Employee object", () => {
+test("create a new Employee object", () => {
     const employee = new Employee();
     expect(typeof(employee)).toBe("object");
   });
+
+  test("get employee name", () => {
+    const name = "Liz";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+  });
+  
+  test("get employee id", () => {
+    const testValue = 100;
+    const employee = new Employee("Foo", testValue);
+    expect(employee.id).toBe(testValue);
+  });
+  
+  test("get employee email", () => {
+    const testValue = "email@email.com";
+    const employee = new Employee("Foo", 1, testValue);
+    expect(employee.email).toBe(testValue);
+  });
+  
+  test("get name when I call getName()", () => {
+    const testValue = "Liz";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+  });
+  
