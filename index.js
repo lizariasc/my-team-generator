9fd8d94f947770b377d5547634a8ff82dc27f5ba
LAgrl <lizethariasc14@gmail.com>
2021-06-20 18:21:38 -0700

    add questions

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+function validate(input) {
+    return input !== '';
+}
+
+const questions = {
+    type: function() {
+        return {
+            message: "Which type of team member would you like to add?",
+            type: "list",
+            name: "member",
+            choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+        }
+} 
+}
